a5a78278d4608c9eacfc6b8b74475ba84e6ed6ec
Abstractiox <106570157+Abstractiox@users.noreply.github.com>
2022-10-18 07:17:48 +0800

    Add files via upload

---
+++ jss.js
@@ -0,0 +1,29 @@
+//core links
+
+function FORGE() {
+    window.location.href = "https://files.minecraftforge.net/net/minecraftforge/forge/index_1.18.2.html";
+}
+
+function OPTIFINE() {
+    window.location.href = "https://www.optifine.net/home";
+}
+
+function Curseforge() {
+    window.location.href = "https://www.curseforge.com";
+}
+
+function MCreator() {
+    window.location.href = "https://mcreator.net";
+}
+
+function BBNet() {
+    window.location.href = "https://www.blockbench.net";
+}
+
+
+//mods links
+
+
+function Mod_Something_Mod() {
+    window.location.href = "https://www.mediafire.com/file/uqt3phatx5jow75/Something.jar/file";
+}
